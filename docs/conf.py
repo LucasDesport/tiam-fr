8cfad3e75f0b94d658c9e35f7e664306f4c576fd
LucasDesport <50015318+LucasDesport@users.noreply.github.com>
2024-07-30 18:26:34 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,12 +13,14 @@ author = 'Author One, Author Two, Author Three'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = [
+    'myst_parser',
+    'rst2pdf.pdfbuilder',
+    # other extensions if any
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -26,17 +28,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-
 # -- Options for MyST-Parser -------------------------------------------------
 # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
 
 myst_enable_extensions = ["dollarmath"]
 
-extensions = [
-    'rst2pdf.pdfbuilder',
-    # other extensions
-]
-
+# -- Options for rst2pdf -----------------------------------------------------
 pdf_documents = [
-    ('index', 'MyProject', 'My Project Documentation', 'Author Name'),
+    ('index', 'TIAM-FR', 'TIAM-FR Documentation', 'Author One, Author Two, Author Three'),
 ]
