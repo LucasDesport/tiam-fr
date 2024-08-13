dac775f3de48942917d6d2a9000b685b7c9a079e
LucasDesport <50015318+LucasDesport@users.noreply.github.com>
2024-08-13 08:57:37 +0200

    Update conf.py
    new update for building Latex files

--- docs/conf.py
+++ docs/conf.py
@@ -1,52 +1,50 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'TIAM-FR'
 copyright = '2024, Centre for Applied Mathematics of Mines Paris - PSL'
 author = 'Author One, Author Two, Author Three'
+release = '1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-#extensions = [
-#    'myst_parser',
-#    'rst2pdf.pdfbuilder',
-# other extensions if any
-#]
-
-latex_engine = 'pdflatex'
-latex_elements = {
-    'papersize': 'a4paper',  # Adjust to 'letterpaper' if needed
-    'pointsize': '10pt',
-    # Additional LaTeX configurations if necessary
-}
-
-latex_documents = [
-    ('index', 'TIAM-FR.tex', 'TIAM-FR Documentation', 'Author One, Author Two, Author Three', 'manual'),
+extensions = [
+    'myst_parser',
+    # Other Sphinx extensions
 ]
-
-
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-# -- Options for MyST-Parser -------------------------------------------------
-# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
+# -- Options for LaTeX output ------------------------------------------------
 
-myst_enable_extensions = ["dollarmath"]
+latex_engine = 'pdflatex'  # or 'xelatex' or 'lualatex', depending on your needs
 
-# -- Options for rst2pdf -----------------------------------------------------
-#pdf_documents = [
-#    ('index', 'TIAM-FR', 'TIAM-FR Documentation', 'Author One, Author Two, Author Three'),
-#]
+latex_elements = {
+    'papersize': 'a4paper',  # Options: 'a4paper', 'letterpaper'
+    'pointsize': '10pt',     # Options: '10pt', '11pt', '12pt'
+    'preamble': r'''
+        % Custom LaTeX commands can go here
+        \usepackage{amsmath,amsfonts,amssymb,amsthm}  % For math symbols
+        \usepackage{graphicx}  % For including graphics
+        \usepackage{hyperref}  % For clickable links in the PDF
+    ''',
+    'figure_align': 'htbp',  # Positioning of figures
+    # Additional settings:
+    # 'extraclassoptions': 'openany',  # Remove blank pages between chapters
+    # 'fncychap': '\\usepackage[Bjarne]{fncychap}',  # Custom chapter styling
+}
+
+# Grouping the document tree into LaTeX files. List of tuples.
+# (source start file, target name, title, author, documentclass [howto/manual]).
+latex_documents = [
+    ('index', 'TIAM-FR.tex', 'TIAM-FR Documentation',
+     'Author One, Author Two, Author Three', 'manual'),
+]
+
+# Other LaTeX settings
+latex_show_urls = 'footnote'  # How to display URLs: 'no', 'footnote', 'inline'
