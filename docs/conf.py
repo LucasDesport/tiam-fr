fddb366c7ed4d82c6d60127c9814f96a15280e60
LucasDesport <50015318+LucasDesport@users.noreply.github.com>
2024-11-05 10:46:15 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -47,21 +47,20 @@ latex_engine = 'pdflatex'  # or 'xelatex' or 'lualatex', depending on your needs
 latex_elements = {
     'papersize': 'a4paper',  # Options: 'a4paper', 'letterpaper'
     'pointsize': '10pt',     # Options: '10pt', '11pt', '12pt'
+    'preamble': r'''
+    % Avoid blank pages
+        % \renewcommand{\clearpage}{\ifodd\value{page}\else\hfill\null\newpage\fi}
+        % \usepackage{emptypage}
+        % Custom LaTeX commands can go here
+        \usepackage{amsmath,amsfonts,amssymb,amsthm}  % For math symbols
+        \usepackage{graphicx}  % For including graphics
+        \usepackage{hyperref}  % For clickable links in the PDF
+    ''',
     'figure_align': 'htbp',  # Positioning of figures
-    % Custom LaTeX commands can go here
-    \usepackage{amsmath,amsfonts,amssymb,amsthm}  % For math symbols
-    \usepackage{graphicx}  % For including graphics
-    \usepackage{hyperref}  % For clickable links in the PDF
-}
-    # 'preamble': r'''
-    # % Avoid blank pages
-    #    \renewcommand{\clearpage}{\ifodd\value{page}\else\hfill\null\newpage\fi}
-    #    \usepackage{emptypage}
-
-    #''',
- # Additional settings:
+    # Additional settings:
     # 'extraclassoptions': 'openany',  # Remove blank pages between chapters
     # 'fncychap': '\\usepackage[Bjarne]{fncychap}',  # Custom chapter styling
+}
 
 # Grouping the document tree into LaTeX files. List of tuples.
 # (source start file, target name, title, author, documentclass [howto/manual]).
